025a69eaba723f8d81b7a763c355d5fe4134b31b
kochier <kochier@users.noreply.github.com>
2014-03-01 16:43:02 -0600

    Update backto.php
    Add indenting.

--- library/files/backto.php
+++ library/files/backto.php
@@ -6,19 +6,21 @@ echo '<div class = "backto">';
 $path = $_SERVER["PHP_SELF"];
 $path = substr($path, 1);
 $result = mysqli_query($conn, "SELECT Map, Parent FROM map WHERE Pathname = '$path'");  
-while($row = mysqli_fetch_array($result)){
-$Map = $row['Map'];
-$Parent =$row['Parent'];
-}
+while($row = mysqli_fetch_array($result))
+    {
+    $Map = $row['Map'];
+    $Parent =$row['Parent'];
+    }
 while($Parent > 0)
-{
-$result2 = mysqli_query($conn, "SELECT Pathname, Name, Parent, Map FROM map WHERE Child = '$Map'");
-while($row = mysqli_fetch_array($result2)){
-$Map = $row['Map'];
-$Parent = $row['Parent'];
-echo '<a  href="http://' .$_SERVER['SERVER_NAME'] .'/' .$row['Pathname'] .'">Back To '.$row['Name'] .'</a><br />';
-}
-}
+    {
+    $result2 = mysqli_query($conn, "SELECT Pathname, Name, Parent, Map FROM map WHERE Child = '$Map'");
+while($row = mysqli_fetch_array($result2))
+        {
+        $Map = $row['Map'];
+        $Parent = $row['Parent'];
+        echo '<a  href="http://' .$_SERVER['SERVER_NAME'] .'/' .$row['Pathname'] .'">Back To '.$row['Name'] .'</a><br />';
+        }
+    }
 echo '</div>';
 include 'closedb.php';
 ?>
